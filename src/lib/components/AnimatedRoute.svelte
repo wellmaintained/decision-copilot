4fc790c2d09fdc5c73f3b381d27dff499cc7d37f
David Laing <david@mechanical-orchard.com>
2023-08-09 10:32:05 +0000

    Add Animated route transitions

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { fade, fly } from 'svelte/transition';
+	import { page } from '$app/stores';
+</script>
+
+{#key $page.url}
+	<div in:fly={{ y: 200, duration: 700 }}>
+		<slot />
+	</div>
+{/key}
