eeb0f21642cd1d6def8e0eb06478c6c5edff8b34
David Laing <david@mechanical-orchard.com>
2023-08-09 10:41:26 +0000

    Removed files in .firebase

--- .firebase/decision-copilot/functions/entries/pages/_page.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<h1 class="text-green-600" data-svelte-h="svelte-1vckeb7">Welcome to SvelteKit</h1> <p data-svelte-h="svelte-jl9sbz">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p> <button class="btn btn-primary" data-svelte-h="svelte-7xkr8l">Click me!</button>`;
-});
-export {
-  Page as default
-};
