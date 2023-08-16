4e8937c2270a7c353c5febeaf665b41d8a905d4c
David Laing <david@mechanical-orchard.com>
2023-08-16 11:14:42 +0000

    Add a decision list

--- src/routes/decision/+page.svelte
+++ src/routes/decision/+page.svelte
@@ -1,1 +1,39 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { db } from '$lib/firebase';
+	import { collection, query, where, getDocs } from 'firebase/firestore';
+	import { user } from '$lib/firebase';
+
+	let decisions: any[] = [];
+
+	onMount(async () => {
+		const q = query(collection(db, 'decisions'), where('user', '==', $user?.uid));
+		const querySnapshot = await getDocs(q);
+		decisions = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+	});
+</script>
+
 <h1>Decision list</h1>
+
+{#if $user}
+	{#if decisions.length > 0}
+		<table class="table w-full">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Description</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each decisions as decision}
+					<tr>
+						<td>{decision.name}</td>
+						<td>{decision.description}</td>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	{:else}
+		<p>No decisions found.</p>
+	{/if}
+{/if}
