5e13da346594131d1fb78059f7e3747d713d95b4
David Laing <david@mechanical-orchard.com>
2023-08-16 11:37:03 +0000

    aider: Update decision summary page to use ID from URL for editing decisions.
    # Aider chat conversation:
    
    USER: the decision summary page should use the id in the url to select which decision it is editing
    
    ASSISTANT: To modify the decision summary page so that it uses the ID in the URL to select which decision it is editing, I will update the `src/routes/decision/summary/+page.svelte` file. Here is the diff for the change:
    
    ```diff
    --- src/routes/decision/summary/+page.svelte
    +++ src/routes/decision/summary/+page.svelte
    @@ ... @@
     <script lang="ts">
     	import { db, user } from '$lib/firebase';
    -	import { collection, doc, setDoc, getDoc } from 'firebase/firestore';
    +	import { collection, doc, setDoc, getDoc, updateDoc } from 'firebase/firestore';
    +	import { page } from '$app/stores';
    
     	let name = '';
     	let description = '';
     	const decisionsRef = collection(db, 'decisions');
    +	let decisionId: string;
    
    +	$: decisionId = $page.params.id;
    +
    +	onMount(async () => {
    +		if (decisionId) {
    +			const decisionRef = doc(decisionsRef, decisionId);
    +			const docSnap = await getDoc(decisionRef);
    +			if (docSnap.exists()) {
    +				const data = docSnap.data();
    +				name = data.name;
    +				description = data.description;
    +			} else {
    +				console.error('No such decision!');
    +			}
    +		}
    +	});
    +
     	async function saveDecision() {
     		try {
    -			const decisionRef = doc(decisionsRef, 'DEC-1');
    -			await setDoc(decisionRef, {
    +			const decisionRef = decisionId ? doc(decisionsRef, decisionId) : doc(decisionsRef);
    +			const data = {
     				name: name,
     				description: description,
     				user: $user!.uid
     			};
    +			if (decisionId) {
    +				await updateDoc(decisionRef, data);
    +			} else {
    +				await setDoc(decisionRef, data);
    +			}
     			const docSnap = await getDoc(decisionRef);
     			console.log('Decision saved: ', docSnap.data());
     		} catch (e) {
     			console.error('Error adding decision: ', e);
     		}
     	}
     </script>
    ```
    
    This diff adds the necessary imports and logic to fetch the decision data based on the ID from the URL when the page is mounted. It also modifies the `saveDecision` function to either update an existing decision if an ID is present or create a new one if not.

--- src/routes/decision/summary/+page.svelte
+++ src/routes/decision/summary/+page.svelte
@@ -1,19 +1,43 @@
 <script lang="ts">
 	import { db, user } from '$lib/firebase';
-	import { collection, doc, setDoc, getDoc } from 'firebase/firestore';
+	import { collection, doc, setDoc, getDoc, updateDoc } from 'firebase/firestore';
+	import { page } from '$app/stores';
 
 	let name = '';
 	let description = '';
 	const decisionsRef = collection(db, 'decisions');
+	let decisionId: string;
+
+	$: decisionId = $page.params.id;
+
+	onMount(async () => {
+		if (decisionId) {
+			const decisionRef = doc(decisionsRef, decisionId);
+			const docSnap = await getDoc(decisionRef);
+			if (docSnap.exists()) {
+				const data = docSnap.data();
+				name = data.name;
+				description = data.description;
+			} else {
+				console.error('No such decision!');
+			}
+		}
+	});
 
 	async function saveDecision() {
 		try {
-			const decisionRef = doc(decisionsRef, 'DEC-1');
-			await setDoc(decisionRef, {
+			const decisionRef = decisionId ? doc(decisionsRef, decisionId) : doc(decisionsRef);
+			const data = {
 				name: name,
 				description: description,
 				user: $user!.uid
 			});
+			};
+			if (decisionId) {
+				await updateDoc(decisionRef, data);
+			} else {
+				await setDoc(decisionRef, data);
+			}
 			const docSnap = await getDoc(decisionRef);
 			console.log('Decision saved: ', docSnap.data());
 		} catch (e) {
