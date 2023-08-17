0909a8f56106ac71762ebd4037031d63ce61ef6d
David Laing <david@mechanical-orchard.com>
2023-08-17 23:23:20 +0000

    Only show steps when editing a decision detail

--- src/routes/decision/+layout.svelte
+++ src/routes/decision/+layout.svelte
@@ -2,42 +2,47 @@
 	import { page } from '$app/stores';
 	import AnimatedRoute from '$lib/components/AnimatedRoute.svelte';
 	import AuthCheck from '$lib/components/AuthCheck.svelte';
+
+	$: decisionId = $page.params.decisionId;
 </script>
 
 <AuthCheck>
-	<nav class="flex justify-center my-6">
-		<ul class="steps">
-			<a href="/decision" class="step step-primary">Decision List</a>
-			<a
-				href="/decision/summary"
-				class="step"
-				class:step-primary={$page.route.id?.match(/summary|matrix|details|workflow/g)}
-			>
-				Decision Summary
-			</a>
-			<a
-				href="/decision/matrix"
-				class="step"
-				class:step-primary={$page.route.id?.match(/matrix|details|workflow/g)}
-			>
-				Decision Matrix
-			</a>
-			<a
-				href="/decision/details"
-				class="step"
-				class:step-primary={$page.route.id?.match(/details|workflow/g)}
-			>
-				Details
-			</a>
-			<a
-				href="/decision/workflow"
-				class="step"
-				class:step-primary={$page.route.id?.includes('workflow')}
-			>
-				Workflow
-			</a>
-		</ul>
-	</nav>
+	{#if decisionId}
+		<nav class="flex justify-center my-6">
+			<ul class="steps">
+				<a href="/decision" class="step step-primary">Decision List</a>
+
+				<a
+					href="/decision/${decisionId}/summary"
+					class="step"
+					class:step-primary={$page.route.id?.match(/summary|matrix|details|workflow/g)}
+				>
+					Decision Summary
+				</a>
+				<a
+					href="/decision/${decisionId}/matrix"
+					class="step"
+					class:step-primary={$page.route.id?.match(/matrix|details|workflow/g)}
+				>
+					Decision Matrix
+				</a>
+				<a
+					href="/decision/${decisionId}/details"
+					class="step"
+					class:step-primary={$page.route.id?.match(/details|workflow/g)}
+				>
+					Details
+				</a>
+				<a
+					href="/decision/${decisionId}/workflow"
+					class="step"
+					class:step-primary={$page.route.id?.includes('workflow')}
+				>
+					Workflow
+				</a>
+			</ul>
+		</nav>
+	{/if}
 
 	<AnimatedRoute>
 		<div class="card w-4/6 mx-auto bg-base-100">
