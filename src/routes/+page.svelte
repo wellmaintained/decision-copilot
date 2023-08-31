a01d396576c1deb1386bbea3acd0b231e48e6dc4
David Laing <david@mechanical-orchard.com>
2023-08-31 13:58:41 +0100

    Add home page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,23 @@
-<h1 class="text-green-600">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button class="btn btn-primary">Click me!</button>
+<div class="hero bg-base-100 shadow-md">
+  <div class="hero-content flex-col lg:flex-row-reverse">
+    <img src="/making-a-great-decision.webp" alt="A team making a great decision" class="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+        <h1 class="text-3xl font-bold">Decision Copilot helps teams make <em>great</em> decisions</h1>
+        <p class="pt-5">A <em>good decision</em> is one where stakeholders:</p>
+        <ul class="pl-3 list-inside list-disc">
+            <li>Understand what the decision is about</li>
+            <li>Are clear how the decision will be made</li>
+            <li>Have participated in making the decision (according to their role) and</li>
+            <li>Can access details of the decision once it has been made (both now and in the future)</li>
+        </ul>
+        <p class="pt-5"><em>Great</em> decisions are made:</p>
+        <ul class="pl-3 list-inside list-disc">
+            <li>Quickly</li>
+            <li>With a high likelihood of being correct</li>
+        </ul>
+        <p class="pt-5">Decision Copilot guides teams through the process of making decisions together; 
+            providing structure and removing toil to enable great decision making</p>
+        <a class="mt-5 btn btn-primary" href="/decision">Get Started</a>
+    </div>
+  </div>
+</div>
