14a7fc8833c7e13e4635542743370da325e0e2b8
David Laing <david@mechanical-orchard.com>
2023-08-31 23:41:35 +0100

    Add decision field

--- src/routes/decision/[decisionId]/decide/+page.svelte
+++ src/routes/decision/[decisionId]/decide/+page.svelte
@@ -4,11 +4,26 @@
 	import { page } from '$app/stores';
 	import type { Decision } from '$lib/types';
 	import DecisionOptions from '$lib/components/DecisionOptions.svelte';
+	import MarkdownEditor from '$lib/components/MarkdownEditor.svelte';
+	import { updateDoc } from 'firebase/firestore';
 
     const decisionId = $page.params.decisionId;
    	const decisionStore = docStore<Decision>(firestore, `decisions/${decisionId}`);
+
+	function handleDecisionUpdate(e: CustomEvent) {
+		const newDecision = e.detail;
+		updateDoc(decisionStore.ref!, {
+			decision: newDecision
+		});
+    }
 </script>
 
-<h2>Decide</h2>
+<h1 class="font-bold text-2xl">Decide</h1>
 
 <DecisionOptions {decisionStore}/>
+
+<h2 class="font-semibold text-xl">Decision</h2>
+<MarkdownEditor 
+	value={$decisionStore?.decision} 
+	on:blur={handleDecisionUpdate} 
+/>
