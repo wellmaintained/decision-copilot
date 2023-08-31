6500691a8e607497fbf8be52a6a960725382d6c6
David Laing <david@mechanical-orchard.com>
2023-08-31 11:27:19 +0100

    Move decision options to decide page

--- src/routes/decision/[decisionId]/decide/+page.svelte
+++ src/routes/decision/[decisionId]/decide/+page.svelte
@@ -1,4 +1,14 @@
 <script lang="ts">
+    import { docStore } from 'sveltefire';
+	import { firestore } from '$lib/firebase';
+	import { page } from '$app/stores';
+	import type { Decision } from '$lib/types';
+	import DecisionOptions from '$lib/components/DecisionOptions.svelte';
+
+    const decisionId = $page.params.decisionId;
+   	const decisionStore = docStore<Decision>(firestore, `decisions/${decisionId}`);
 </script>
 
 <h2>Decide</h2>
+
+<DecisionOptions {decisionStore}/>
