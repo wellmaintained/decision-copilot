7d4ca3a33fb71834ed87e49b50c7a0ff4a54e155
David Laing <david@mechanical-orchard.com>
2023-08-31 09:38:31 +0100

    Delete Option button

--- src/routes/decision/[decisionId]/summary/+page.svelte
+++ src/routes/decision/[decisionId]/summary/+page.svelte
@@ -5,8 +5,6 @@
       doc, updateDoc, writeBatch } from 'firebase/firestore';
 	import { page } from '$app/stores';
 	import type { Decision, User } from '$lib/types';
-	import DecisionOption from '$lib/components/DecisionOption.svelte';
-    import { writable } from "svelte/store";
 	import { onMount } from 'svelte';
 
 	const decisionId = $page.params.decisionId;
@@ -69,6 +67,12 @@
 			});
 		await batch.commit();
     }
+
+	async function deleteOption(option: any) {
+		await updateDoc(decisionRef, {
+			options: arrayRemove(option)
+		});
+	}
   
 	onMount(async () => {
 		const element = document.getElementById('decision_description') as HTMLTextAreaElement;
@@ -131,7 +135,7 @@
 		</div>
 		{#each decisionOptions as option, index (option.id)}
 		<label class="input input-bordered flex items-center gap-2">
-			<span class="label-text text-neutral-content">{index+1}</span>
+			<span class="label-text text-neutral-content cursor-move">{index+1}</span>
 			<input
 				type="text"
 				class="grow"
@@ -139,6 +143,22 @@
 				placeholder="Describe the option in a few words"
 				on:blur={(event) => updateOption(option, event)}
 			/>
+			<button class="btn btn-ghost btn-xs" on:click={() => deleteOption(option)}>
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-4 h-4"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+					/>
+				</svg>
+			</button>
 		</label>
 		{/each}
 	</div>
