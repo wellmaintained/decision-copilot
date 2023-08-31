7bbd337fb23ef22e036637f937810e47201389fb
David Laing <david@mechanical-orchard.com>
2023-08-31 11:30:28 +0100

    Change order

--- src/routes/decision/[decisionId]/identify/+page.svelte
+++ src/routes/decision/[decisionId]/identify/+page.svelte
@@ -87,7 +87,6 @@
 			id="decision_description"
 		></textarea>
 	</label>
-	<DecisionCriteria {decisionStore}/>
 	<div class="flex flex-col gap-2">
 		<div class="text-neutral-content">Reversibility - <em>like choosing a</em></div>
 		<div class="input input-bordered h-max">
@@ -108,6 +107,7 @@
 		{/each}
 		</div>
 	</div>
+	<DecisionCriteria {decisionStore}/>
 	<div class="flex flex-col gap-2">
 		<div class="text-neutral-content">Stakeholders - <em>who has an interest in - or is impacted by - this decision?</em></div>
 		<div class="input input-bordered h-max">
