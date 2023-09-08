b45732a1f30f33ea766ef2aae57b350caef77d75
David Laing <david@mechanical-orchard.com>
2023-09-08 21:22:09 +0100

    Extend emails that are allowed to register

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,31 @@
 import { HttpsError, beforeUserCreated } from 'firebase-functions/v2/identity';
 import * as logger from 'firebase-functions/logger';
 
+function isAllowed(email: string | undefined): boolean {
+	if (!email) {
+		return false;
+	}
+	const allowedValues = [
+		'@mechanical-orchard.com',
+		'scottmuc@gmail.com',
+		'wendy@castlelaing.com',
+		'mrdavidlaing@gmail.com'
+	];
+	for (const allowed of allowedValues) {
+		if (email.includes(allowed)) {
+			return true;
+		}
+	}
+	return false;
+}
+
 export const beforecreated = beforeUserCreated((event) => {
 	const user = event.data;
-	if (!user?.email?.includes('@mechanical-orchard.com')) {
+	if (!isAllowed(user?.email)) {
 		logger.info("Blocking registration because email isn't from recognised domain", { user: user });
-		throw new HttpsError('invalid-argument', 'Unauthorized email');
+		throw new HttpsError(
+			'invalid-argument',
+			"Registration blocked because email isn't from recognised domain"
+		);
 	}
 });
--- src/index.test.ts
+++ src/index.test.ts
@@ -4,4 +4,23 @@ describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
 		expect(1 + 2).toBe(3);
 	});
+
+	it('finds allowed email in email whitelist', () => {
+		expect(isAllowed('foo@bar.com')).toBeTruthy();
+		expect(isAllowed('jack@example.com')).toBeTruthy();
+		expect(isAllowed('jill@example.com')).toBeFalsy();
+	});
 });
+
+function isAllowed(email: string | undefined): boolean {
+	if (!email) {
+		return false;
+	}
+	const allowedValues = ['jack@example.com', '@bar.com'];
+	for (const allowed of allowedValues) {
+		if (email.includes(allowed)) {
+			return true;
+		}
+	}
+	return false;
+}
