b9b83ab0e09c1c41b0074ae57b94a9aff608281b
David Laing <david@mechanical-orchard.com>
2023-09-09 17:37:04 +0100

    Validate Stakeholder Assignment

--- src/lib/components/StakeholderAvatar.svelte
+++ src/lib/components/StakeholderAvatar.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="flex flex-row items-center gap-2">
-    <div class="avatar w-8 h-8 rounded-full overflow-hidden border-base-300 border">
+    <div class="avatar w-8 h-8 rounded-full overflow-hidden border-base-300 border flex-none">
         <img alt="Avatar for {stakeholder.displayName}" src={stakeholder.photoURL.toString()} />
     </div>
     <div class="label-text w-max">{stakeholder.displayName}</div>
--- src/routes/decision/[decisionId]/process/+page.svelte
+++ src/routes/decision/[decisionId]/process/+page.svelte
@@ -10,42 +10,97 @@
 	
 	const decisionRepo = getContext<DecisionRepo>('decisionRepo');
 	const decisionData = decisionRepo.latestDecisionData;
-
-    let openAccordianStep:string = "select";
-    if ($decisionData?.decisionMethod) {
-        openAccordianStep = "assign";
-    }
-    let stakeholdersWithRoleCount = 0;
-    for (let stakeholder of $decisionData?.stakeholders || []) {
-        if (stakeholder.role) {
-            stakeholdersWithRoleCount++;
-        }
-    }
-    if (stakeholdersWithRoleCount === $decisionData?.stakeholders?.length) {
-        openAccordianStep = "schedule";
-    }
-
     let involvedStakeholders = writable([] as Stakeholder[]);
-    decisionData.subscribe(async (value) => {
-        if (value?.stakeholders && value?.stakeholders.length>0) {
-            involvedStakeholders.set(await decisionRepo.fetchDecisionStakeholderData($decisionData?.stakeholders));
+    let roleAssignmentErrors = writable([] as string[]);
+    let openAccordianStep = writable("assign");
+    
+    validateRoleAssignment();
+
+    decisionData.subscribe(async (d) => {
+        if (d?.stakeholders && d.stakeholders.length>0) {
+            involvedStakeholders.set(await decisionRepo.fetchDecisionStakeholderData(d.stakeholders));
         }
     });
 
-	async function handleDecisionMethodSelect(decisionMethod: string): Promise<void> {
+    async function handleDecisionMethodSelect(decisionMethod: string): Promise<void> {
 		await decisionRepo.updateDecisionField('decisionMethod', decisionMethod);
-        openAccordianStep = "assign";
-        openAccordianStep = openAccordianStep;
+        $openAccordianStep = "assign";
 	}
+
+    function validateRoleAssignment(): boolean {
+        $roleAssignmentErrors = [];
+        if ($decisionData?.stakeholders && $decisionData?.stakeholders.length>0) {
+            if ($decisionData.stakeholders.filter((s) => !s.role).length > 0) {
+                $roleAssignmentErrors.push("Every stakeholder must have a role assigned");
+            }
+            if ($decisionData.stakeholders.filter((s) => s.role === 'decider').length === 0) {
+                $roleAssignmentErrors.push("At least 1 stakeholder must be assigned the Decider role");
+            } 
+        }
+        if ($roleAssignmentErrors.length > 0) {
+            $openAccordianStep = "assign";
+            return false;
+        }
+        return true;
+    }
 
 	function handleStakeholderRoleChange(stakeholder_id: string, role: string): void {
         decisionRepo.updateStakeholderRole(stakeholder_id, role);
+	}
+
+	function handleAssignRolesNext(_: Event) {
+        if (validateRoleAssignment()) {
+            $openAccordianStep = "schedule";
+        }
 	}
 </script>
 
 <div class="join join-vertical w-full">
     <div class="collapse collapse-arrow join-item border border-base-300">
-        <input type="radio" name="step-accordian" checked={openAccordianStep=="select"} /> 
+        <input type="radio" name="step-accordian" checked={$openAccordianStep=="assign"} /> 
+        <div class="collapse-title text-xl font-medium">
+            Assign roles
+        </div>
+        <div class="collapse-content"> 
+            <div class="flex flex-col gap-2">
+                <div class="input input-bordered h-max">
+                    {#each $involvedStakeholders as stakeholder, index (stakeholder.id)}
+                        <div class="flex flex-row flex-wrap pt-3 align-middle" class:border-t={ index > 0 }>
+                            <div class="flex-initial w-1/4">
+                                <StakeholderAvatar {stakeholder} />
+                            </div>
+                            <div class="flex-auto">
+                                <RadioButtonOptions 
+                                        options={[
+                                            {key: "decider", label: "Decider", explaination: "The person making the decision"},
+                                            {key: "advisor", label: "Advisor", explaination: "The person providing advice"},
+                                            {key: "observer", label: "Observer", explaination: "The person being informed"}
+                                        ]}
+                                        selected={$decisionData?.stakeholders?.find((s) => s.stakeholder_id === stakeholder.id)?.role}
+                                        on:changeOption={(e) => handleStakeholderRoleChange(stakeholder.id, e.detail)}
+                                    />
+                            </div>
+                        </div>
+                    {:else}
+                        <div class="loading loading-dots loading-md"></div>
+                    {/each}
+                    {#if $roleAssignmentErrors.length > 0}
+                        <div role="alert" class="alert alert-error mt-3 mb-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                                <ul>
+                                    {#each $roleAssignmentErrors as error}
+                                        <li>{error}</li>
+                                    {/each}
+                                </ul>
+                        </div>
+                    {/if}
+                </div>
+                <button class="btn btn-primary w-1/4 ml-auto mr-2" on:click={handleAssignRolesNext}>Next</button>
+            </div>
+        </div>
+    </div>
+    <div class="collapse collapse-arrow join-item border border-base-300">
+        <input type="radio" name="step-accordian" checked={$openAccordianStep=="select"} /> 
     <div class="collapse-title">
         <h2 class="text-xl font-medium">
             Decision making method
@@ -83,41 +138,9 @@
         </div>
     </div>
   </div>
-  <div class="collapse collapse-arrow join-item border border-base-300">
-    <input type="radio" name="step-accordian" checked={openAccordianStep=="assign"} /> 
-    <div class="collapse-title text-xl font-medium">
-      Assign roles
-    </div>
-    <div class="collapse-content"> 
-      <div class="flex flex-col gap-2">
-			<div class="input input-bordered h-max">
-                <table class="table w-full"><tbody>
-					{#each $involvedStakeholders as stakeholder (stakeholder.id)}
-                        <tr>
-                            <td><StakeholderAvatar {stakeholder} /></td>
-                            <td>
-                                <RadioButtonOptions 
-                                        options={[
-                                            {key: "decider", label: "Decider", explaination: "The person making the decision"},
-                                            {key: "advisor", label: "Advisor", explaination: "The person providing advice"},
-                                            {key: "observer", label: "Observer", explaination: "The person being informed"}
-                                        ]}
-                                        selected={$decisionData?.stakeholders?.find((s) => s.stakeholder_id === stakeholder.id)?.role}
-                                        on:changeOption={(e) => handleStakeholderRoleChange(stakeholder.id, e.detail)}
-                                    />
-                            </td>
-                        </tr>
-					{:else}
-            			<tr><td><span class="loading loading-dots loading-md"></span></td></tr>
-					{/each}
-				</tbody></table>
-			</div>
-            <button class="btn btn-primary w-1/4 ml-auto mr-2" on:click={(_) => openAccordianStep="schedule"}>Next</button>
-		</div>
-    </div>
-  </div>
+  
     <div class="collapse collapse-arrow join-item border border-base-300">
-    <input type="radio" name="step-accordian" checked={openAccordianStep=="schedule"} /> 
+    <input type="radio" name="step-accordian" checked={$openAccordianStep=="schedule"} /> 
     <div class="collapse-title text-xl font-medium">
       Schedule
     </div>
