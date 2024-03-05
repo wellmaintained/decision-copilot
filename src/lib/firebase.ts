46f79416e8068cccd35bedb66731f8ed10a9a24f
David Laing <david@mechanical-orchard.com>
2024-03-05 11:55:18 +0000

    initial FKIT based app

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCdxfhEXgjcvIFF5GUUnFwnAscUj4HNsMY",
+    authDomain: "decision-copilot.firebaseapp.com",
+    projectId: "decision-copilot",
+    storageBucket: "decision-copilot.appspot.com",
+    messagingSenderId: "698337109435",
+    appId: "1:698337109435:web:1c6a069dcf70a3469bd09e",
+    measurementId: "G-FVPGZL8NDE"
+  };
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth();
+export const storage = getStorage();
